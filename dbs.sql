dde74e654d4d663888de35c3880cc7a6c75141a4
pursuit <67893254+unique-pure@users.noreply.github.com>
2022-01-25 13:37:52 +0800

    Update dbs.sql

--- dbs.sql
+++ dbs.sql
@@ -41,8 +41,6 @@ CREATE TABLE `t_absencerecord`  (
 -- ----------------------------
 -- Records of t_absencerecord
 -- ----------------------------
-INSERT INTO `t_absencerecord` VALUES (1, '5720201402', '陈冠华', '软件工程学院', '软件开发181班', 6, '6611', '2020-01-01', '而非');
-INSERT INTO `t_absencerecord` VALUES (2, '5720201404', '刘建龙', '能源与机械学院', '软件开发181班', 6, '6608', '2021-01-06', '晚上学习王辉亲了饶请伟若群');
 
 -- ----------------------------
 -- Table structure for t_academy
@@ -59,9 +57,7 @@ CREATE TABLE `t_academy`  (
 -- ----------------------------
 -- Records of t_academy
 -- ----------------------------
-INSERT INTO `t_academy` VALUES (1, '软件工程学院', 0);
-INSERT INTO `t_academy` VALUES (2, '能源与机械学院', 0);
-INSERT INTO `t_academy` VALUES (3, '商学院', 0);
+
 
 -- ----------------------------
 -- Table structure for t_admin
@@ -77,7 +73,6 @@ CREATE TABLE `t_admin`  (
 -- ----------------------------
 -- Records of t_admin
 -- ----------------------------
-INSERT INTO `t_admin` VALUES ('admin', '超级管理员', '111');
 
 -- ----------------------------
 -- Table structure for t_applyinrecord
@@ -96,13 +91,6 @@ CREATE TABLE `t_applyinrecord`  (
 -- ----------------------------
 -- Records of t_applyinrecord
 -- ----------------------------
-INSERT INTO `t_applyinrecord` VALUES (1, '陈冠沙发', 4, '发送', '2021-01-08 14:37:48', 1);
-INSERT INTO `t_applyinrecord` VALUES (2, '沙发', 3, '发啊生', '2021-01-08 14:43:52', 0);
-INSERT INTO `t_applyinrecord` VALUES (3, '还款卡', 3, '加不加', '2021-01-08 15:11:59', 0);
-INSERT INTO `t_applyinrecord` VALUES (4, '李顺根', 1, '阿萨法', '2021-01-09 14:30:28', 0);
-INSERT INTO `t_applyinrecord` VALUES (5, '李鸿章', 1, '沙发', '2021-01-09 14:35:50', 0);
-INSERT INTO `t_applyinrecord` VALUES (6, '啊啊', 1, '阿发', '2021-01-09 14:46:03', 0);
-INSERT INTO `t_applyinrecord` VALUES (7, '', 1, '发顺丰', '2021-01-09 14:46:25', 1);
 
 -- ----------------------------
 -- Table structure for t_changeroomrecord
@@ -123,8 +111,6 @@ CREATE TABLE `t_changeroomrecord`  (
 -- ----------------------------
 -- Records of t_changeroomrecord
 -- ----------------------------
-INSERT INTO `t_changeroomrecord` VALUES (1, '5720181423', '王顺根', '6611', '5511', '2021-01-08 14:26:29', '士大夫撒', 1);
-INSERT INTO `t_changeroomrecord` VALUES (2, '5720181423', '王顺根', '5511', '4411', '2021-01-09 15:16:11', '想换个地方', 0);
 
 -- ----------------------------
 -- Table structure for t_dorm
@@ -166,12 +152,6 @@ CREATE TABLE `t_dormmaster`  (
 -- ----------------------------
 -- Records of t_dormmaster
 -- ----------------------------
-INSERT INTO `t_dormmaster` VALUES ('000001', '陈思敏', '000001', '女', '13907944444', 1, 0);
-INSERT INTO `t_dormmaster` VALUES ('000002', '程军', '000002', '男', '91110', 2, 1);
-INSERT INTO `t_dormmaster` VALUES ('000003', '李华', '000003', '男', '432431', 6, 1);
-INSERT INTO `t_dormmaster` VALUES ('000004', '李焕英', '000004', '女', '123455', 4, 0);
-INSERT INTO `t_dormmaster` VALUES ('000005', '刘东海', '000005', '男', '6876886', 5, 0);
-INSERT INTO `t_dormmaster` VALUES ('000006', '程欢欢', '000006', '女', '5221', 3, 0);
 
 -- ----------------------------
 -- Table structure for t_repairpeople
@@ -188,7 +168,6 @@ CREATE TABLE `t_repairpeople`  (
 -- ----------------------------
 -- Records of t_repairpeople
 -- ----------------------------
-INSERT INTO `t_repairpeople` VALUES ('001', '王工', '111', 0);
 
 -- ----------------------------
 -- Table structure for t_repairrecord
@@ -207,10 +186,6 @@ CREATE TABLE `t_repairrecord`  (
 -- ----------------------------
 -- Records of t_repairrecord
 -- ----------------------------
-INSERT INTO `t_repairrecord` VALUES (1, '陈冠华', '6611', '2021-01-07', '上厕所把厕所堵了', 1);
-INSERT INTO `t_repairrecord` VALUES (2, '杨永琪', '6611', '2021-01-07', '阿发', 1);
-INSERT INTO `t_repairrecord` VALUES (3, '离去南', '6611', '2021-01-08', '沙发坏了', 1);
-INSERT INTO `t_repairrecord` VALUES (4, '刘淑仪', '6611', '2021-01-08', '第三方分散', 0);
 
 -- ----------------------------
 -- Table structure for t_room
@@ -273,28 +248,5 @@ CREATE TABLE `t_student`  (
 -- ----------------------------
 -- Records of t_student
 -- ----------------------------
-INSERT INTO `t_student` VALUES ('5720191408', '何志飞', '5720191408', '男', '133338', '江西景德镇', '软件工程学院', '软件开发191班', 5, '5529', 0);
-INSERT INTO `t_student` VALUES ('5720191425', '孙欣', '5720191425', '女', '18807911234', '北京', '软件工程学院', '软件开发191班', 3, '3323', 0);
-INSERT INTO `t_student` VALUES ('5720191427', '李焕英', '5720191426', '女', '123455', '江西永丰', '商学院', '软件造价191班', 4, '4215', 0);
-INSERT INTO `t_student` VALUES ('5720191434', '尚志浩', '5720191434', '男', '188882', '江西赣州', '软件工程学院', '软件开发191班', 5, '5430', 0);
-INSERT INTO `t_student` VALUES ('5720191530', '陈思敏', '5720191530', '女', '13907944444', '江苏南京', '商学院', '软件会计191班', 1, '1425', 0);
-INSERT INTO `t_student` VALUES ('5720191533', '程潇', '5720191533', '女', '17756', '江西上饶', '软件工程学院', '软件电商191班', 1, '1403', 0);
-INSERT INTO `t_student` VALUES ('5720191908', '刘薇', '5720191908', '女', '1233445', '江西南昌', '能源与机械学院', '能源动力191班', 3, '3423', 0);
-INSERT INTO `t_student` VALUES ('5720201401', '蔡伟', '5720201401', '男', '13907933333', '湖南娄底', '软件工程学院', '软件开发201班', 6, '6529', 0);
-INSERT INTO `t_student` VALUES ('5720201402', '陈冠华', '5720181402', '男', '3325325', '江西吉安', '软件工程学院', '软件开发201班', 6, '6611', 0);
-INSERT INTO `t_student` VALUES ('5720201403', '李华', '5720201403', '男', '432431', '江西吉安', '软件工程学院', '软件开发201班', 6, '6611', 0);
-INSERT INTO `t_student` VALUES ('5720201404', '刘建龙', '5720201404', '男', '313212412', '江西川水', '能源与机械学院', '软件电气201班', 6, '6608', 0);
-INSERT INTO `t_student` VALUES ('5720201405', '刘璋', '5720201405', '男', '3121414', '江西吉安', '能源与机械学院', '能源动力201班', 6, '6611', 0);
-INSERT INTO `t_student` VALUES ('5720201411', '张国瑞', '5720201411', '男', '13807922222', '湖北荆州', '软件工程学院', '虚拟现实201班', 6, '6213', 0);
-INSERT INTO `t_student` VALUES ('5720201424', '魏延', '5720201424', '男', '313131', '江西临川', '软件工程学院', '软件开发201班', 3, '6609', 0);
-INSERT INTO `t_student` VALUES ('5720201425', '戴鑫', '5720201425', '女', '3121314211', '江西吉安', '商学院', '软件电商201班', 1, '1706', 0);
-INSERT INTO `t_student` VALUES ('5720201426', '杨永琪', '5720182026', '男', '1421414', '江西吉安', '软件工程学院', '软件开发201班', 6, '6611', 1);
-INSERT INTO `t_student` VALUES ('5720201427', '杨永琪', '5720120427', '男', '21312312', '甘肃天水', '软件工程学院', '软件开发201班', 6, '6611', 0);
-INSERT INTO `t_student` VALUES ('5720211303', '陈冠熙', '5720211303', '男', '18907011111', '福建厦门', '软件工程学院', '机器人211班', 2, '2234', 0);
-INSERT INTO `t_student` VALUES ('5720211431', '尹一平', '5720211431', '男', '11199', '山西太原', '软件工程学院', '软件开发211班', 5, '5123', 0);
-INSERT INTO `t_student` VALUES ('5720211611', '黄景林', '5720211611', '男', '1456790', '广东深圳', '能源与机械学院', '软件电气211班', 5, '5411', 0);
-INSERT INTO `t_student` VALUES ('5720211622', '程欢欢', '5720211622', '女', '5221', '浙江杭州', '商学院', '电子商务211班', 3, '3101', 0);
-INSERT INTO `t_student` VALUES ('5720211632', '杨慧慧', '5720211632', '女', '5220', '内蒙古', '商学院', '电子商务211班', 3, '3101', 0);
-INSERT INTO `t_student` VALUES ('5720211712', '程军', '5720211712', '男', '91110', '广西南宁', '商学院', '电子商务211班', 2, '2421', 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
